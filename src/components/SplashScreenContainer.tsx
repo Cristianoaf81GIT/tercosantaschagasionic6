a5b57fc9fafe61114b32fbc1f745e3118b2770e6
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-04-29 14:14:50 -0300

    added splash html/css splash screen

---
+++ src/components/SplashScreenContainer.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import './SplashScreenContainer.css';
+import { useHistory } from 'react-router-dom';
+import { IonProgressBar } from '@ionic/react';
+
+const SplashScreen: React.FC = () => {
+    
+    const [progress, setProgress] = useState<number>(0.0);
+    const history = useHistory();
+    const TIMEOUT = 0.1666666666666666666666666666667;
+    const goToHomeScreen: Function = (): void => {
+        setTimeout(() => {
+           history.push('/page/Inbox')
+        }, 2000)
+    }
+
+    const changeProgressBar: Function = (): void => {
+        const interval = setTimeout(() => {
+            setProgress( ( progress + TIMEOUT ))
+        }, 100)
+        
+        if ( progress >= 0.9999999999999999 ) {
+            clearTimeout( interval );
+            goToHomeScreen()            
+        }
+        
+    }
+
+    useEffect(() => {
+        changeProgressBar();        
+    },[progress])
+
+    return(
+        <div className="container-splash">
+            <img src='../../assets/logo.jpg'/>
+            
+            <div className="progress-container">
+                <IonProgressBar color="danger" type="determinate" value={progress}/>
+            </div>         
+        </div>
+    )
+};
+
+export default SplashScreen;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,7 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
+import SplashScreen from '../components/SplashScreenContainer';
 import './Page.css';
 
 const Page: React.FC = () => {
@@ -8,15 +9,7 @@ const Page: React.FC = () => {
   const { name } = useParams<{ name: string; }>();
 
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    <IonPage>      
 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
@@ -24,7 +17,26 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        {
+          name === 'splash' 
+          ? <SplashScreen/> 
+          : (
+              <div>
+                <IonHeader>
+                <IonToolbar>
+                  <IonButtons slot="start">
+                    <IonMenuButton />
+                  </IonButtons>
+                  <IonTitle>{name}</IonTitle>
+                </IonToolbar>
+                </IonHeader>
+
+                <ExploreContainer name={name} />
+
+              </div>
+            )
+        }
+        
       </IonContent>
     </IonPage>
   );
